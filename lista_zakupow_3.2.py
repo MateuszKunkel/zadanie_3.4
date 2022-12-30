ea905cde57ec507ff1facab5fc3d1f131ef1aa5f
Mateusz Kunkel <matiqk@wp.pl>
2022-12-30 18:35:45 +0100

    add loop

--- lista_zakupow_3.2.py
+++ lista_zakupow_3.2.py
@@ -2,3 +2,11 @@ slownik = {
     "piekarnia" : ["chleb", "bułki", "pączek"],
     "warzywniak" : ["marchew", "seler", "rukola"]
 }
+
+print("Lista zakupów:")
+
+suma_zakupow = 0
+
+for i in slownik:
+    suma_zakupow += len(slownik.items())
+    print(f"Idę do {i.capitalize()}, kupuję tu następujące rzeczy: {', '.join([a.title() for a in slownik.items()])}")
