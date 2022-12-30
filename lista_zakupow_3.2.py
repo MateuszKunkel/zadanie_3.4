7d80637cb2b32c76fd3f0a2c12301ecf107f4f8f
Mateusz Kunkel <matiqk@wp.pl>
2022-12-30 18:38:15 +0100

    add printsum

--- lista_zakupow_3.2.py
+++ lista_zakupow_3.2.py
@@ -10,3 +10,5 @@ suma_zakupow = 0
 for i in slownik:
     suma_zakupow += len(slownik.items())
     print(f"Idę do {i.capitalize()}, kupuję tu następujące rzeczy: {', '.join([a.title() for a in slownik.items()])}")
+
+print(f"W sumie kupiłem {suma_zakupow} rzeczy.")
